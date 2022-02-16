3162c4aa8767efbc84eef070e6c7e18547a60621
sgwps <7010504@gmail.com>
2022-02-17 01:01:03 +0300

    registration_test

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Test(models.Model):
+    test2 = models.CharField(max_length=1, default='y')
+    test3 = models.CharField(max_length=1, default='y')
--- user_profile/views.py
+++ user_profile/views.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
-
+from templates import *
 # Create your views here.
+def test_reg(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = User.objects.create_user(username=username, password=password)
+        user.save()
+    return render(request, 'reg_test.html')
