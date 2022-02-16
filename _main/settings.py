26e7ae4a1faf62fc17718496742a1a692d93a454
sgwps <7010504@gmail.com>
2022-02-17 01:08:13 +0300

    apps added to list, templates dirs created

--- _main/settings.py
+++ _main/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'news',
+    'user_profile'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = '_main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['user_profile/templates', '_templates', 'news/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
