84291fc436b43861b663fec88ab4a77f875f7f1b
Jericho2k <grustam1357@gmail.com>
2022-02-17 17:26:54 +0300

    added news and profile (first version)

--- news/models.py
+++ news/models.py
@@ -1,3 +1,18 @@
+from tkinter.tix import Tree
 from django.db import models
 
 # Create your models here.
+class NewsApp(models.Model):
+    news = models.CharField(max_length = 200)
+
+class Post(models.Model):
+    newsapp = models.ForeignKey(NewsApp, on_delete = models.CASCADE)
+    paragraph = models.TextField(blank = True, null = True)
+
+    
+
+class PostImage(models.Model):
+    post = models.ForeignKey(Post, on_delete = models.CASCADE)
+    image = models.ImageField(null = True, blank = True, upload_to = "images/")
+    
+    
--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    email = models.EmailField('Email', unique = True)
+    user_name = models.CharField('Username', max_length = 150, unique = True)
+    first_name = models.CharField('First name', max_length = 150, unique = True)
+    second_name = models.CharField('Second name',max_length = 150, unique = True)
+
+class ProfileApp(models.Model):
+    user = newsapp = models.OneToOneField(User, on_delete = models.CASCADE)
+    role = models.CharField()
+    
+
+        
