c750d9d7032d2ce6d950a624c044144926f5d913
sgwps <7010504@gmail.com>
2022-02-18 19:06:33 +0300

    format_html

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.utils.html import format_html
+
 from templates import *
 # Create your views here.
 def test_reg(request):
@@ -8,4 +10,6 @@ def test_reg(request):
         password = request.POST['password']
         user = User.objects.create_user(username=username, password=password)
         user.save()
-    return render(request, 'reg_test.html')
+    content = {'text' : format_html("This is normal text - <b>and this is bold text</b>.")}
+
+    return render(request, 'reg_test.html', content)
