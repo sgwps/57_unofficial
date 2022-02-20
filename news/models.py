d157780f499dbfd7ddd1c65f5d7c8344805ba0bb
sgwps <7010504@gmail.com>
2022-02-20 18:18:00 +0300

    article model final version

--- news/models.py
+++ news/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 
 class Article(models.Model):
-    headline = models.CharField(blank=False, max_length=200)
-    date_created = models.DateField(blank=False)
-    date_uploaded = models.DateField(blank=False)
+    headline = models.CharField(max_length=200)
+    date_created = models.DateField()
+    date_uploaded = models.DateField(blank=True)
     preview_image = models.ImageField(upload_to='preview', blank=True)
     preview_text = models.CharField(blank=True)
-    article_content = models.CharField()  #json_dir
+    content_empty = models.BooleanField()
+    article_content = models.CharField(blank=True)  #json
 # authors - many to many (position, news, userid)
 
 
