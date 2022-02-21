865f2010157ca2ab186b80dca7e821758ae58b34
sgwps <7010504@gmail.com>
2022-02-21 12:26:36 +0300

    user models finished

--- user_profile/models.py
+++ user_profile/models.py
@@ -4,58 +4,10 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 # Create your models here.
-'''
-class User(models.Model):
-    email = models.EmailField('Email', unique = True)
-    user_name = models.CharField('Username', max_length = 150, unique = True)
-    first_name = models.CharField('First name', max_length = 150, unique = True)
-    second_name = models.CharField('Second name',max_length = 150, unique = True)
-
-    def __str__(self):
-        return self.user_name
-
-class ProfileApp(models.Model):
-    user = models.OneToOneField(User, on_delete = models.CASCADE)
-    role = models.CharField()
-
-
-'''
-
-class OurUser(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    ID = models.IntegerField()
-    userName = models.CharField(max_length=100)
-    Email = models.EmailField()
-    ifConfirmed = models.BooleanField()
-    firstName = models.CharField(max_length=50)
-    lastName = models.CharField(max_length=50)
-    BIO = models.CharField(max_length=250)
-    birthday = models.DateField()
-    position = models.ForeignKey(to=Position, on_delete=models.SET_NULL(), related_name='Position')
-    subject = models.ForeignKey(to=Subject, on_delete=models.SET_NULL(), related_name='Subject')
-    Class = models.ForeignKey(to=Class, on_delete=models.SET_NULL(), related_name='Class')
-    mediaPost = models.IntegerField()
-    city = models.IntegerField()
-
-# Magic from Habr section link for original topic: https://habr.com/ru/post/313764/
-
-@receiver(post_save, sender=User)
-def create_user_(sender, instance, created, **kwargs):
-    if created:
-        OurUser.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_(sender, instance, **kwargs):
-    instance.profile.save()
-
-
-
-
-
 
 class Position(models.Model):
     ID = models.IntegerField()
-    Title = models.CharField(max_length=30)
+    Name = models.CharField(max_length=30)
 
 
 class Subject(models.Model):
@@ -63,17 +15,49 @@ class Subject(models.Model):
     Name = models.CharField(max_length=30)
 
 
-class Classes(models.Model):
-    ID = models.IntegerField()
-    GraduationYear = models.DateField()
-    Profile =  models.ForeignKey(to=Class, on_delete=models.SET_NULL(), related_name='Specializations')
-    Letter = models.CharField(max_length=5)
-
-
-class Spec(models.Model):
+class Specialization(models.Model):
     # Specialization
     ID = models.IntegerField()
     Name = models.CharField(max_length=30)
+
+
+class Class(models.Model):
+    ID = models.IntegerField()
+    GraduationYear = models.IntegerField()
+    Specialization = models.ForeignKey(to=Specialization, on_delete=models.SET_NULL(), related_name='Specializations')
+    Letter = models.CharField(max_length=1)
+
+
+class Profile(models.Model):
+    GENDER_CHOICES = (
+        ('m', 'male'),
+        ('f', 'female'),
+        ('n', 'neutral')
+    )
+    UserID = models.OneToOneField(User, on_delete=models.CASCADE)
+    Gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    isConfirmed = models.BooleanField()
+    BIO = models.CharField()
+    Birthday = models.DateField(blank=True)
+    Position = models.ForeignKey(to=Position, on_delete=models.PROTECT(), related_name='Position')
+    Subject = models.ForeignKey(to=Subject, on_delete=models.PROTECT(), related_name='Subject')
+    Class = models.ForeignKey(to=Class, on_delete=models.PROTECT(), related_name='Class')
+    mediaPost = models.IntegerField()
+    city = models.IntegerField()
+
+# Magic from Habr section link for original topic: https://habr.com/ru/post/313764/
+
+
+@receiver(post_save, sender=User)
+def create_user_(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_(sender, instance, **kwargs):
+    instance.profile.save()
+
 
 
 
