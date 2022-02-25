467d5b57668f7820232c22261da544a581e3d254
LE <lugobzonS@yandex.com>
2022-02-26 00:28:08 +0300

    News Prototype

--- news/views.py
+++ news/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+import json
 # Create your views here.
+
+
+def Get_News(request):
+    f = str(request.id) +".json"
+    with open(f) as data:
+        ctx = json.load(data)
+    return render(request, 'newsPage.html', context = ctx)
+
