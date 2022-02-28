71b53192277fec5aa086d76c60c8a08d9274f19a
sgwps <7010504@gmail.com>
2022-02-28 14:11:15 +0300

    user models, still in progress

--- user_profile/models.py
+++ user_profile/models.py
@@ -5,47 +5,45 @@ from django.dispatch import receiver
 
 # Create your models here.
 
-class Position(models.Model):
-    ID = models.IntegerField()
+
+class Subject(models.Model):
     Name = models.CharField(max_length=30)
 
 
-class Subject(models.Model):
-    ID = models.IntegerField()
+class MediaPost(models.Model):
     Name = models.CharField(max_length=30)
 
 
 class Specialization(models.Model):
-    # Specialization
-    ID = models.IntegerField()
     Name = models.CharField(max_length=30)
+    Description = models.CharField(max_length=1000)
 
 
-class Class(models.Model):
-    ID = models.IntegerField()
+class Grade(models.Model):
     GraduationYear = models.IntegerField()
-    Specialization = models.ForeignKey(to=Specialization, on_delete=models.SET_NULL(), related_name='Specializations')
+    Specialization = models.ForeignKey(to=Specialization, on_delete=models.PROTECT(), related_name='Class')
     Letter = models.CharField(max_length=1)
 
 
 class Profile(models.Model):
-    GENDER_CHOICES = (
+    GENDERS = (
         ('m', 'male'),
         ('f', 'female'),
         ('n', 'neutral')
     )
+    is_teacher = models.BooleanField()
+    is_student = models.BooleanField()
+    is_manager = models.BooleanField()
+    is_mediaStaff = models.BooleanField()
     UserID = models.OneToOneField(User, on_delete=models.CASCADE)
-    Gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    Gender = models.CharField(max_length=1, choices=GENDERS)
     isConfirmed = models.BooleanField()
     BIO = models.CharField()
     Birthday = models.DateField(blank=True)
-    Position = models.ForeignKey(to=Position, on_delete=models.PROTECT(), related_name='Position')
-    Subject = models.ForeignKey(to=Subject, on_delete=models.PROTECT(), related_name='Subject')
-    Class = models.ForeignKey(to=Class, on_delete=models.PROTECT(), related_name='Class')
-    mediaPost = models.IntegerField()
     city = models.IntegerField()
 
 # Magic from Habr section link for original topic: https://habr.com/ru/post/313764/
+# разбить на классы по Positions???
 
 
 @receiver(post_save, sender=User)
@@ -57,6 +55,27 @@ def create_user_(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_(sender, instance, **kwargs):
     instance.profile.save()
+
+
+class Teacher(models.Model):
+    ProfileID = models.OneToOneField(Profile, on_delete=models.CASCADE)
+    Subject = models.ForeignKey(to=Subject, on_delete=models.PROTECT(), related_name='Subject')  #many to many - remake
+    working_currently = models.BooleanField()
+
+
+class Student(models.Model):
+    ProfileID = models.OneToOneField(Profile, on_delete=models.CASCADE)
+    Grade = models.ForeignKey(to=Grade, on_delete=models.PROTECT(), related_name='Class')
+
+
+class Manager(models.Model):
+    post = models.CharField(max_length=50)
+    working_currently = models.BooleanField()
+
+
+class MediaStaff(models.Model):
+    post = models.CharField(max_length=50)
+
 
 
 
