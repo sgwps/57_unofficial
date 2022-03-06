67e6e42aff2d0f63571833d80cf42dd7d91a337d
Kopikok <gleelu@yandex.ru>
2022-03-06 12:51:35 +0300

    Added RichTextFormField and code refactoring

--- _main/urls.py
+++ _main/urls.py
@@ -17,9 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from news.views import *
 from news_creation.views import *
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main_page, name='Главная'),
     path('news_creation/', creation, name='Создание новости'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- news_creation/forms.py
+++ news_creation/forms.py
@@ -1,8 +1,19 @@
 import imp
 from typing import Any
 from django import forms
+from ckeditor.fields import RichTextFormField
+
 
 class NewsCreationForm(forms.Form):
-    header = forms.CharField(max_length=200, label="Введите заголовок")
-    img = forms.ImageField(label='Вставьте картинку', required=False, error_messages={'invalid':("Image files only")}, widget=forms.FileInput)
-    paragraph = forms.CharField(label="Параграф", widget=forms.Textarea)
+    header = forms.CharField(
+        max_length=200,
+        label="Заголовок статьи",
+        widget=forms.TextInput(attrs={'class': 'form-control'}
+    ))
+    img = forms.ImageField(
+        label="Значок статьи",
+        required=False,
+        error_messages={'invalid':("Image files only")},
+        widget=forms.FileInput(attrs={'class': 'form-control', 'type': 'file', 'id': 'formFile'}
+    ))
+    body = RichTextFormField(label="Текст статьи")
