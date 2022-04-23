b242e971c39c3205728d26c950abcdb795b53094
Viacheslav Savchenko <viacheslav_savchenko@epam.com>
2022-04-23 15:14:02 +0300

    Formatting

--- user_profile/models.py
+++ user_profile/models.py
@@ -5,7 +5,7 @@ from django.dispatch import receiver
 from datetime import datetime
 
 
-def GetAll(Model):    #  for Subject, MediaPost, Specialization, Gender
+def GetAll(Model):  # for Subject, MediaPost, Specialization, Gender
     result = list()
     model_list = Model.objects.all()
     for item in model_list:
@@ -13,12 +13,15 @@ def GetAll(Model):    #  for Subject, MediaPost, Specialization, Gender
             result.append((item.id, item.name))
     return tuple(result)
 
+
 def GetAllInDict(Model):
+
     result = GetAll(Model)
     result_dict = dict()
     for i in result:
         result_dict[i[0]] = i[1]
     return result_dict
+
 
 class Specialization(models.Model):
     name = models.CharField(max_length=40, null=True)
@@ -31,18 +34,21 @@ class Subject(models.Model):
 class MediaPost(models.Model):
     name = models.CharField(max_length=40, null=True)
 
+
 class Gender(models.Model):  # ISO 5218
     name = models.CharField(max_length=20, null=True)
 
 
 class Grade(models.Model):
     graduation_year = models.IntegerField()
-    specialization = models.ForeignKey(to=Specialization, on_delete=models.PROTECT, related_name='grades', null=True)
+    specialization = models.ForeignKey(
+        to=Specialization, on_delete=models.PROTECT, related_name='grades', null=True)
     letter = models.CharField(max_length=1, null=True, blank='True')
 
     @staticmethod
     def GetGradesByYear(year):
-        letters = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И', 'К', 'Л', 'М', 'Н' ,'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Э', 'Ю', 'Я']
+        letters = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И', 'К', 'Л', 'М', 'Н',
+                   'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Э', 'Ю', 'Я']
         result = list()
         grades_list = Grade.objects.filter(graduation_year=year)
         for grade in grades_list:
@@ -57,8 +63,8 @@ class Grade(models.Model):
                 tuple[1][1] = ""
             result.append(tuple)
         res_dict = {
-            "grades":result,
-            "letters":letters
+            "grades": result,
+            "letters": letters
         }
         return res_dict
 
@@ -73,11 +79,10 @@ class Grade(models.Model):
             grade.graduation_year = data['graduation_year']
             if data.get('grade', {}).get('custom_grade_letter') != None:
                 grade.letter = data.get('grade', {}).get('custom_grade_letter')
-                grade.specialization = Specialization.objects.get(name=data['grade']['specialization'])
+                grade.specialization = Specialization.objects.get(
+                    name=data['grade']['specialization'])
             grade.save()
             return grade
-
-
 
 
 '''
@@ -94,7 +99,8 @@ def create_user(data):
     first_name = data["first_name"]
     last_name = data["last_name"]
     username = data["username"]
-    user = User.objects.create_user(email=email, password=password, first_name=first_name, last_name=last_name, username=username)
+    user = User.objects.create_user(
+        email=email, password=password, first_name=first_name, last_name=last_name, username=username)
     user.save()
     return user
 
@@ -104,16 +110,17 @@ class Profile(models.Model):
     is_student = models.BooleanField(default=False)
     is_manager = models.BooleanField(default=False)
     is_media_staff = models.BooleanField(default=False)
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profiles')
-    gender = models.ForeignKey(to=Gender, on_delete=models.PROTECT, related_name='users', null=True)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='profiles')
+    gender = models.ForeignKey(
+        to=Gender, on_delete=models.PROTECT, related_name='users', null=True)
     is_confirmed = models.BooleanField(default=False)
     bio = models.CharField(blank=True, max_length=1000)
     birthday = models.DateField(blank=True, null=True)
     city = models.IntegerField(blank=True, null=True)
 
-
     @staticmethod
-    def create(data):  #data - json or dict??
+    def create(data):  # data - json or dict??
         print(data)
         user = create_user(data['user'])
         user.save()
@@ -121,7 +128,8 @@ class Profile(models.Model):
         profile.user = user
         print("----", int(data["profile"].get("gender")))
         try:
-            profile.gender = Gender.objects.get(pk=int(data["profile"].get("gender")))
+            profile.gender = Gender.objects.get(
+                pk=int(data["profile"].get("gender")))
         except:
             profile.gender = None
         birthday = data["profile"].get("birthday")
@@ -139,7 +147,8 @@ class Profile(models.Model):
 
 class Teacher(models.Model):
     subjects = models.ManyToManyField(Subject)
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='teacher')
+    profile = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name='teacher')
 
     @staticmethod
     def create(data, profile):
@@ -156,9 +165,10 @@ class Teacher(models.Model):
 
 
 class Student(models.Model):
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='student')
-    grade = models.ForeignKey(to=Grade, on_delete=models.PROTECT, related_name='students', blank=True, null=True)
-
+    profile = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name='student')
+    grade = models.ForeignKey(
+        to=Grade, on_delete=models.PROTECT, related_name='students', blank=True, null=True)
 
     @staticmethod
     def create(data, profile):
@@ -167,7 +177,6 @@ class Student(models.Model):
         if data.get('graduation_year') != None:
             grade = Grade.GetGrade(data)
         student.save()
-
 
 
 class Manager(models.Model):
@@ -179,5 +188,3 @@ class Manager(models.Model):
 class MediaStaff(models.Model):
     ProfileID = models.OneToOneField(Profile, on_delete=models.CASCADE)
     post = models.CharField(max_length=50)
-
-
