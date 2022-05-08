ca9cf55c20221ece8be0c5f457c88f6e66049a97
sgwps <7010504@gmail.com>
2022-05-08 23:16:25 +0300

    fixes

--- news_creation/admin.py
+++ news_creation/admin.py
@@ -2,14 +2,3 @@ from django.contrib import admin
 from .models import Article
 
 # Register your models here.
-# Это необязательно, но сделано для проверки, ссылка на гит где нашёл :
-# https://github.com/LeeHanYeong/django-quill-editor
-from django.contrib import admin
-from .models import Article
-
-# Register your models here.
-# Это необязательно, но сделано для проверки, ссылка на гит где нашёл :
-# https://github.com/LeeHanYeong/django-quill-editor
-@admin.register(Article)
-class QuillPostAdmin(admin.ModelAdmin):
-    pass 
