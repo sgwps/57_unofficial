214a28ea2a84782d9ce3725e8df55452fa4612fa
sgwps <7010504@gmail.com>
2022-05-14 17:59:32 +0300

    database

--- messanger/models.py
+++ messanger/models.py
@@ -3,13 +3,11 @@ from user_profile.models import  User
 # Create your models here.
 
 class Chat(models.Model):
-    id = models.IntegerField(blank=True, null=True)
-    user_1 = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=true)
-    user_2 = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=true)
+    users = models.ManyToManyField(to=User, on_delete=models.SET_NULL, null=True)  # on_delete
+    name = models.CharField(max_length=300, blank=True, null=True)
 
 
 class Message(models.Model):
-    id = models.IntegerField(blank=True, null = True)
-    chat_id =  models.ForeignKey(to=Chat,on_delete=models.SET_NULL,null = true)
-    sender = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=true)
+    chat_id =  models.ForeignKey(to=Chat,on_delete=models.SET_NULL)
+    sender = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True, blank=True)
     content  = models.CharField(max_length=300)
