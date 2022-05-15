2794c664c93714ee58fcda6a072dc865c8445f1f
Kopikok <gleelu@yandex.ru>
2022-05-15 17:12:42 +0300

    Added commentaries

--- news/models.py
+++ news/models.py
@@ -1,20 +1,15 @@
 from django.db import models
-
-
-from django.db import models
 from django.urls import clear_script_prefix
 from user_profile import models as user_models
+
 
 class Article(models.Model):
     content = models.TextField()
     date_created = models.DateTimeField()
 
 
-
-
 class Comment(models.Model):
-    artcile = models.ForeignKey(to=Article, on_delete=models.CASCADE, related_name="comments")
-    parent_id = models.ForeignKey(to='self', on_delete=models.CASCADE, related_name="comments", blank=True, null=True)
-    user_id = models.ForeignKey(to=user_models.User, on_delete=models.SET_NULL, related_name="comments", blank=True, null=True)
-    content = models.TextField()
-    date_created = models.DateTimeField()
+    article = models.ForeignKey(to=Article, on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey(to=user_models.User, on_delete=models.SET_NULL, related_name="comments", blank=True, null=True)
+    content = models.CharField(max_length=256)
+    date_created = models.DateTimeField(auto_now_add=True)
